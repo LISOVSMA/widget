b5890ed3f61383fc53bea62309cd90529930b39f
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-04-30 14:50:28 +0300

    adds code

---
+++ src/js/widget.js
@@ -0,0 +1,47 @@
+const wetherBlock = document.querySelector('#weather');
+
+async function loadWeather(e) {
+  wetherBlock.innerHTML = `
+  <div class="weather__loading">
+    <img src="/images/loading.gif" alt="loading..." />
+  </div>`;
+
+  const server =
+    'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kharkiv&appid=1492ab3abea0bcc790a5243042e94c71';
+  const response = await fetch(server, {
+    method: 'GET',
+  });
+  const responseResult = await response.json();
+  if (response.ok) {
+    getWeather(responseResult);
+  } else {
+    wetherBlock.innerHTML = responseResult.message;
+  }
+}
+
+function getWeather(data) {
+  const location = data.name;
+  const temp = Math.round(data.main.temp);
+  const feelsLike = Math.round(data.main.feels_like);
+  const weatherStatus = data.weather[0].main;
+  const weatherIcon = data.weather[0].icon;
+
+  const template = `
+  <div class="weather__header">
+    <div class="weather__main">
+    <div class="weather__city">${location}</div>
+    <div class="weather__status">${weatherStatus}</div>
+    </div>
+    <div class="weather__icon">
+        <img src="https://api.openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}" />
+    </div>
+    </div>
+    <div class="weather__temp">${temp}</div>
+    <div class="weather__feels-like">Feels-like: ${feelsLike}</div>`;
+
+  wetherBlock.innerHTML = template;
+}
+
+if (wetherBlock) {
+  loadWeather();
+}
